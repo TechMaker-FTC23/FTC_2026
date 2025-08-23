a7cb00c524e6963bb6b2a33b289d751508f13d5a
Johnny Dreher <johnnydreher@gmail.com>
2025-08-23 10:51:31 -0300

    Inicialização de valores zerados nas constantes

--- TeamCode/src/main/java/techmaker/util/DataStorage.java
+++ TeamCode/src/main/java/techmaker/util/DataStorage.java
@@ -3,6 +3,6 @@ import com.pedropathing.localization.Pose;
 
 import techmaker.constants.Constants.Intake.SampleColor;
 public class DataStorage {
-    public static SampleColor allianceColor;
-    public static Pose robotPose;
+    public static SampleColor allianceColor = SampleColor.Idle;
+    public static Pose robotPose = new Pose(0,0,0);
 }
